714ddc4454d2e21c03614754a7e131413853aaa0
Ashok Kota <ashokkota2005@gmail.com>
2025-09-22 21:26:03 +0530

    Delete 0033-search-in-rotated-sorted-array directory

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        if target not in nums:
-            return -1
-        for i in range(0,len(nums)):
-            if nums[i] == target:
-                return i
-                
-        
